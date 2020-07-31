024345284582e9b8f6666e1c9bf765071106fb9b
kuan <a831202a@gmail.com>
2020-07-31 16:27:47 +0800

    第三天 Q4 90%

--- backend/th.php
+++ backend/th.php
@@ -5,21 +5,56 @@
 <input type="text" name="mid_name" id="mid_name"><button onclick="addMid()">新增</button>
 
 </div>
-<div style="height: 407px; overflow:auto;">
-<table class="all type-list" ></table>
-</div>
+<table class="all type-list"></table>
 
+
+<h2 class="ct">商品管理</h2>
+<div class="ct"><button onclick="location.href='?do=add_goods'">新增商品</button></div>
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>庫存量</td>
+        <td>狀態</td>
+        <td >操作</td>
+    </tr>
+    <?php
+    $goods=$Goods->all();
+    foreach($goods as $g){
+    ?>
+    <tr class="pp">
+        <td class="ct" ><?=$g['no'];?></td>
+        <td><?=$g['name'];?></td>
+        <td class="ct"><?=$g['stock'];?></td>
+        <td class="ct" id="g<?=$g['id'];?>">
+        <?=($g['sh']==1)?"販售中":"已下架";?>
+        </td>
+        <td class="ct">
+        <button onclick="location.href='?do=edit_goods&id=<?=$g['id'];?>'">修改</button>
+        <button onclick="del('goods',<?=$g['id'];?>)">刪除</button>
+        <button onclick="sh(<?=$g['id'];?>,1)">上架</button>
+        <button onclick="sh(<?=$g['id'];?>,2)">下架</button>
+        </td>
+    </tr>
+
+    <?php
+        }
+
+    ?>
+</table>
 
 
 <script>
 getTypeList()
 getBigOption()
+
 function addBig(){
     $.post("api/save_type.php",{'name':$("#big").val(),'parent':0},function(){
         getBigOption()
         getTypeList()
     })
 }
+
 function edit(id){
     let newName=prompt("請輸入要修改的分類名稱",$("#t"+id).html())
     if(newName!=null){
@@ -27,23 +62,39 @@ function edit(id){
         $.post("api/edit_type.php",{id,newName})
     }
 }
+
 function getBigOption(){
     $.get("api/get_big.php",function(options){
             $("#mid").html(options)
     })
 }
+
 function addMid(){
     let name=$("#mid_name").val();
     let big=$("#mid").val();
-    console.log(name,big);
+
     $.post("api/save_type.php",{'name':name,'parent':big},function(){
         getBigOption()
         getTypeList()
     })
 }
+
 function getTypeList(){
     $.get("api/get_type_list.php",function(list){
         $(".type-list").html(list)
+    })
+}
+
+function sh(id,type){
+    $.post("api/sh.php",{id,type},function(){
+        switch(type){
+            case 1:
+                $("#g"+id).html('販售中')
+            break;
+            case 2:
+                $("#g"+id).html('已下架')
+            break;
+        }
     })
 }
 </script>
